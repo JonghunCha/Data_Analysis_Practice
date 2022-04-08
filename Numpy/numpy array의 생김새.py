43f4e56b1e00eaff4f9cb9b903906a69a1909f93
JonghunCha <jonghun0210.bb@gmail.com>
2022-04-08 22:00:43 +0900

    upload numpy array의 생김새.py

---
+++ Numpy/numpy array의 생김새.py
@@ -0,0 +1,16 @@
+import numpy
+
+array1 = numpy.array([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])
+print(array1)
+print(type(array1))
+print(array1.shape)
+print("\n")
+
+array2 = numpy.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+print(array2)
+print(type(array2))
+print(array2.shape)
+print("\n")
+
+print(array1.size)
+print(array2.size)
