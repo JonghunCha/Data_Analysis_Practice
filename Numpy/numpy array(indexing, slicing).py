15a7c80fedc896135296982eefd58f87451ef574
JonghunCha <jonghun0210.bb@gmail.com>
2022-04-09 01:02:36 +0900

    upload numpy array(indexing, slicing).py

--- Numpy/numpy array(indexing, slicing).py
+++ Numpy/numpy array(indexing, slicing).py
@@ -1,10 +1,20 @@
-import unittest
+import numpy
 
+#numpy array도 파이썬 리스트처럼 인덱스를 통해 접근 가능하다
+array1 = numpy.array([1, 2, 3, 4, 5, 6, 7, 8])
+print(array1[0])
+print(array1[3])
+print(array1[-1])
+print(array1[-3])
+print(array1[[1, 3, 5]])
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+#numpy array를 numpy array의 인덱스로 사용할 수 있다
+array2 = numpy.array([2, 4, 6])
+print(array1[array2])
 
-
-if __name__ == '__main__':
-    unittest.main()
+#numpy array도 파이썬 리스트처럼 슬라이싱이 가능하다
+print(array1[3:5])
+print(array1[:7])
+print(array1[4:])
+print(array1[3:-2])
+print(array1[0:8:2])
