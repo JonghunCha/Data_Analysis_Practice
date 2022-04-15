1c299748b012045801d7002ddcd45e9216c52a35
JonghunCha <jonghun0210.bb@gmail.com>
2022-04-15 21:08:32 +0900

    move annotation location

--- pandas/pandas(read_csv).py
+++ pandas/pandas(read_csv).py
@@ -1,3 +1,7 @@
+"""
+1. pandas의 read_csv()는 csv파일의 첫 줄을 column의 이름으로 사용한다. 만약 csv의 첫 줄에 column의 이름이 없다면 아래와 같이 작성해야한다.
+    iphone_dataframe = pandas.read_csv("./iphone.csv", header=None)
+"""
 import pandas
 
 #read_csv()를 이용하여 csv파일을 읽어 DataFrame으로 저장
@@ -9,9 +13,3 @@ print("\n")
 #제품명을 DataFrame의 index로 사용하기
 iphone_dataframe2 = pandas.read_csv("data/iphone.csv", index_col=0)
 print(iphone_dataframe2)
-
-"""
-주의 : pandas의 read_csv()는 csv파일의 첫 줄을 column의 이름으로 사용한다.
-만약 csv의 첫 줄에 column의 이름이 없다면 아래와 같이 작성해야한다.
-iphone_dataframe = pandas.read_csv("./iphone.csv", header=None)
-"""
