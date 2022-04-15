9869375d6d7dc57b6b4c7d2e5bad32ef8fd28071
JonghunCha <jonghun0210.bb@gmail.com>
2022-04-15 21:07:39 +0900

    move annotation location

--- seaborn/kdeplot.py
+++ seaborn/kdeplot.py
@@ -1,3 +1,7 @@
+"""
+1. seaborn.kdeplot()의 경우 pandas.plot()과 다르게 정렬을 해주지 않아도 잘 나오는 것으로 보임
+2. bw(bandwidth)는 smoothing parameter로 bw가 커질수록 큰 너비에 대해, bw가 작을수록 좁은 너비에 대해 각각의 smoothing이 진행되는 것으로 보임
+"""
 import pandas
 import seaborn
 import matplotlib.pyplot
@@ -18,8 +22,3 @@ matplotlib.pyplot.show()
 #키에 대해 KDE(with bw=0.5)
 seaborn.kdeplot(df["Height"], bw=0.5)
 matplotlib.pyplot.show()
-
-"""
-1. seaborn.kdeplot()의 경우 pandas.plot()과 다르게 정렬을 해주지 않아도 잘 나오는 것으로 보임
-2. bw(bandwidth)는 smoothing parameter로 bw가 커질수록 큰 너비에 대해, bw가 작을수록 좁은 너비에 대해 각각의 smoothing이 진행되는 것으로 보임
-"""
